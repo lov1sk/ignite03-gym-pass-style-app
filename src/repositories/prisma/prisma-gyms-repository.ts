3fbbe9bfb525bf2d2a0191ad0636054a5a662ac8
Lucas Ribeiro Santana <lucassantana00@gmail.com>
2023-11-10 12:07:57 -0300

    add: created prisma repository and implemented methods of gymsRepository

--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -1,17 +1,40 @@
-import { Prisma } from "@prisma/client";
+import { Gym, Prisma } from "@prisma/client";
 import { IGymsRepository } from "../gyms-repository";
+import { prisma } from "@/lib/prisma";
 
 export class PrismaGymsRepository implements IGymsRepository {
   async findById(id: string) {
-    throw new Error("Method not implemented.");
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return gym;
   }
   async searchMany(query: string, page: number) {
-    throw new Error("Method not implemented.");
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+    return gyms;
   }
   async findManyNearby(user_latitude: number, user_longitude: number) {
-    throw new Error("Method not implemented.");
+    const nearbyGyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${user_latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${user_longitude}) ) + sin( radians(${user_longitude}) ) * sin( radians( latitude ) ) ) ) <= 10`;
+    return nearbyGyms;
   }
   async create(data: Prisma.GymCreateInput) {
-    throw new Error("Method not implemented.");
+    const createdGym = await prisma.gym.create({
+      data,
+    });
+
+    return createdGym;
   }
 }
