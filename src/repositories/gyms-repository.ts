ebfede580aabfdfa4dc55f6cd18194a4201c9301
Lucas Ribeiro Santana <lucassantana00@gmail.com>
2023-11-13 10:32:33 -0300

    add: created prisma repositories for gyms table

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -3,6 +3,6 @@ import { Gym, Prisma } from "@prisma/client";
 export interface IGymsRepository {
   findById(id: string): Promise<Gym | null>;
   searchMany(query: string, page: number): Promise<Gym[]>;
-  findManyNearby(user_latitude: number, user_longitude: number): Promise<Gym[]>;
+  findManyNearby(latitude: number, longitude: number): Promise<Gym[]>;
   create(data: Prisma.GymCreateInput): Promise<Gym>;
 }
