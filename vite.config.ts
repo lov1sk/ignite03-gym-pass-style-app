a2beea3e708a2de41c022a55fed53b7183c0bf53
Lucas Ribeiro Santana <lucassantana00@gmail.com>
2023-11-13 10:35:26 -0300

    add: create a new config for e2e tests environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    /**
+     * Aqui que definimos que queremos executar os testes de outro ambiente tbm
+     */
+    environmentMatchGlobs: [["src/http/controllers/**", "prisma"]],
+    dir: "src", // Incluir essa linha
+  },
 });
